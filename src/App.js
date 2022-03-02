d98bd1140ca2cee41c2546c5668e811c26ca3215
vitoriaborba <vitoriaborba@icloud.com>
2022-03-02 17:02:47 +0000

    Add Food done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import foodsData from './foods.json';
+import FoodBox from './Components/FoodBox/FoodBox';
+import AddFoodForm from './Components/AddFoodForm/AddFoodForm';
 
 function App() {
+  const [foods, setFood] = useState(foodsData);
+  const [displayFood, setDisplayFood] = useState(foodsData)
+
+  const addNewFood = (newfood) => {
+    const updatedFood = [...foods, newfood]
+    setFood(updatedFood)
+    setDisplayFood(updatedFood)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food List</h1>
+    <AddFoodForm addFood={addNewFood}></AddFoodForm>
+      <hr />
+    {foods.map((food) =>{
+     return (
+     <FoodBox food={food} />
+      )
+    })}
     </div>
   );
 }
---
+++ src/Components/AddFoodForm/AddFoodForm.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+
+function AddFoodForm(props) {
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+    const [calories, setCalories] = useState(0);
+    const [servings, setServings] = useState(0);
+
+    const handleNameInput = (e) => setName(e.target.value);
+    const handleImageInput = (e) => setImage(e.target.value);
+    const handleCaloriesInput = (e) => setCalories(e.target.value);
+    const handleServingsInput = (e) => setServings(e.target.value);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const randomId = Math.random() * 1000000000;
+      const newFood = {
+        _id: randomId,
+        name,
+        image,
+        calories,
+        servings,
+      };
+
+      props.addFood(newFood);
+      setName('');
+      setImage('');
+      setCalories(0);
+      setServings(0);
+    };
+  return (
+    <div>
+      <h2>Add Food</h2>
+      <form onSubmit={handleSubmit}>
+        <label> Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameInput}
+        />
+        <label> Image:</label>
+        <input
+          type="url"
+          name="image"
+          value={image}
+          onChange={handleImageInput}
+        />
+        <label> Calories:</label>
+        <input
+          type="number"
+          name="calories"
+          value={calories}
+          onChange={handleCaloriesInput}
+        />
+        <label> Servings:</label>
+        <input
+          type="number"
+          name="servings"
+          value={servings}
+          onChange={handleServingsInput}
+        />
+
+        <button type="submit"> Add Food</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFoodForm
